2ea8f0e8d9dac2b746d610a927afa837d3013a3f
Brendan Asselstine <mail.asselstine@gmail.com>
2021-07-28 14:10:06 -0700

    Added events.  Removed some dead code.

--- contracts/yield-source/MStableYieldSource.sol
+++ contracts/yield-source/MStableYieldSource.sol
@@ -17,21 +17,51 @@ contract MStableYieldSource is IYieldSource, ReentrancyGuard {
     /// @notice mapping of account addresses to interest-bearing mAsset balances. eg imUSD
     mapping(address => uint256) public imBalances;
 
+    /// @notice Emitted on init
+    /// @param savings The ISavingsContractV2 to bind to
+    event Initialized(ISavingsContractV2 indexed savings);
+
     /// @notice Emitted when asset tokens are supplied to sponsor the yield source
+    /// @param sponsor The address who sponsored
+    /// @param mAssetAmount The amount of deposit token that was sponsored
     event Sponsored(address indexed sponsor, uint256 mAssetAmount);
 
-    constructor(address _savings) ReentrancyGuard() {
+    /// @notice Emitted when asset tokens are supplied to earn yield
+    /// @param from The address who supplied the assets
+    /// @param to The new owner of the assets
+    /// @param amount The amount of assets supplied
+    event Supplied(address indexed from, address indexed to, uint256 amount);
+
+    /// @notice Emitted when asset tokens are redeemed from the yield source
+    /// @param from The address who is redeeming
+    /// @param requestedAmount The amount that was requested to withdraw
+    /// @param actualAmount The actual amount of assets transferred to the address
+    event Redeemed(address indexed from, uint256 requestedAmount, uint256 actualAmount);
+
+    /// @notice Approves max spend by the mAsset
+    /// @param from The user who triggered approve max
+    event ApprovedMax(address indexed from);
+
+    constructor(ISavingsContractV2 _savings) ReentrancyGuard() {
         // As immutable storage variables can not be accessed in the constructor,
         // create in-memory variables that can be used instead.
-        ISavingsContractV2 savingsMemory = ISavingsContractV2(_savings);
-        IERC20 mAssetMemory = IERC20(savingsMemory.underlying());
+        IERC20 mAssetMemory = IERC20(_savings.underlying());
 
         // infinite approve Savings Contract to transfer mAssets from this contract
-        mAssetMemory.safeApprove(_savings, type(uint256).max);
+        mAssetMemory.safeApprove(address(_savings), type(uint256).max);
 
         // save to immutable storage
-        savings = savingsMemory;
+        savings = _savings;
         mAsset = mAssetMemory;
+
+        emit Initialized(_savings);
+    }
+
+    /// @notice Approves of the max spend amount for the Savings contract.
+    function approveMax() public {
+        IERC20(savings.underlying()).safeApprove(address(savings), type(uint256).max);
+
+        emit ApprovedMax(msg.sender);
     }
 
     /// @notice Returns the ERC20 mAsset token used for deposits
@@ -53,6 +83,8 @@ contract MStableYieldSource is IYieldSource, ReentrancyGuard {
         mAsset.safeTransferFrom(msg.sender, address(this), mAssetAmount);
         uint256 creditsIssued = savings.depositSavings(mAssetAmount);
         imBalances[to] += creditsIssued;
+
+        emit Supplied(msg.sender, to, mAssetAmount);
     }
 
     /// @notice Redeems mAsset tokens from the interest-beaing mAsset.
@@ -64,7 +96,7 @@ contract MStableYieldSource is IYieldSource, ReentrancyGuard {
         override
         nonReentrant
         returns (uint256 mAssetsActual)
-    {
+    {   
         uint256 mAssetBalanceBefore = mAsset.balanceOf(address(this));
 
         uint256 creditsBurned = savings.redeemUnderlying(mAssetAmount);
@@ -74,5 +106,7 @@ contract MStableYieldSource is IYieldSource, ReentrancyGuard {
         mAssetsActual = mAssetBalanceAfter - mAssetBalanceBefore;
 
         mAsset.safeTransfer(msg.sender, mAssetsActual);
+
+        emit Redeemed(msg.sender, mAssetAmount, mAssetsActual);
     }
 }
